b8dcc9b79dd098e1b62e4e50ba0c8c1e40ca374d
Olga <Olga@MacBook-Air-Olga.local>
2020-04-29 00:01:06 -0400

    again

--- src/hwork/TestEmployee.java
+++ src/hwork/TestEmployee.java
@@ -6,6 +6,6 @@ public class TestEmployee {
         QAEngineer myEngineer = new QAEngineer("Donna",100, "BestToBest", 50);
         myEngineer.getName();
         System.out.printf("My salary is %f and my company is %s", myEngineer.getSalary(), myEngineer.getCompany());
-        System.out.println("My bonus is usually %f.", myEngineer.getBonus());
+        System.out.printf("My bonus is usually %f.", myEngineer.getBonus());
     }
 }
