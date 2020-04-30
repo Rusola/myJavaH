d516524793c7e75b98eceea43b027852acf8cf08
Olga <ipvola@gmail.com>
2020-04-29 20:14:26 -0400

    comment

--- src/hwork/TestEmployee.java
+++ src/hwork/TestEmployee.java
@@ -2,8 +2,9 @@ package hwork;
 
 public class TestEmployee {
     public static void main(String[] args) {
-        // name of QA Engineer
+
         QAEngineer myEngineer = new QAEngineer("Donna",100, "BestToBest", 50);
+        // name of QA Engineer
         myEngineer.getName();
         System.out.printf("My salary is %f and my company is %s", myEngineer.getSalary(), myEngineer.getCompany());
         System.out.printf("My bonus is usually %f.", myEngineer.getBonus());
